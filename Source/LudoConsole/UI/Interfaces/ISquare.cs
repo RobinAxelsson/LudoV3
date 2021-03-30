4a71014e737fa93725ad300475151a8102774f65
RobinAxelsson <axelsson.robin@outlook.com>
2021-03-30 12:24:08 +0200

    ui library

---
+++ Source/LudoConsole/UI/Interfaces/ISquare.cs
@@ -0,0 +1,18 @@
+﻿using GameEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudoConsole.UI.Interfaces
+{
+    public interface ISquare
+    {
+        public int Index { get; set; }
+        public (int X, int Y) UpperLeft { get; set; }
+        public List<IDrawable> Drawables { get; set; }
+        public List<Pawn> Pawns { get; set; }
+        public List<IDrawable> UpdatePawns();
+    }
+}
---
+++ Source/LudoConsole/UI/Models/Square.cs
@@ -0,0 +1,14 @@
+﻿using GameEngine.Models;
+using System.Collections.Generic;
+
+namespace LudoConsole.UI.Interfaces
+{
+    public class Square : ISquare
+    {
+        public int Index { get; set; }
+        public (int X, int Y) UpperLeft { get; set; }
+        public List<IDrawable> Drawables { get; set; }
+        public List<Pawn> Pawns { get; set; }
+        public List<IDrawable> UpdatePawns() { return new List<IDrawable>(); }
+    }
+}
