844abd68fb68397ad895a99f64653c37912febf7
Albin Alm <almalbin98@gmail.com>
2021-03-31 16:55:37 +0200

    fixed pawn

--- Source/LudoEngine/Models/Pawn.cs
+++ Source/LudoEngine/Models/Pawn.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using LudoEngine.Enum;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using LudoEngine.Board.Classes;
 using LudoEngine.Board.Intefaces;
 
@@ -13,22 +14,19 @@ namespace LudoEngine.Models
         public virtual Game GameID { get; set; }
         public TeamColor Color { get; set; }
         public IGameSquare CurrentSquare { get; set; }
-        public bool Based { get; set; }
+        public bool Based { get; set; } //Kolla om Pawn ligger i basen
         public void Move(int dice)
         {
             var gameSquares = BoardHolder.BoardSquares;
             var tempSquare = CurrentSquare;
             CurrentSquare.Pawns.Remove(this);
-            for (int i = 0; i <= dice; i++)
+            for (var i = 0; i <= dice; i++)
             {
                 tempSquare = BoardHolder.GetNext(gameSquares, tempSquare, Color);
             }
-            foreach (var pawn in tempSquare.Pawns)
+            foreach (var pawn in tempSquare.Pawns.Where(pawn => pawn.Color != Color))
             {
-                if (pawn.Color != Color)
-                {
-                    Eradicate(pawn, tempSquare);
-                }
+                Eradicate(pawn, tempSquare);
             }
             tempSquare.Pawns.Add(this);
         }
